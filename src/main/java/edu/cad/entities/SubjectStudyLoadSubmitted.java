5524a83faed24ea7c3d62982246f060eb79ae3f2
buhaiovos <oleksandr.buhaiov@gmail.com>
2018-12-31 23:14:20 +0200

    adding entities for distributed data

---
+++ src/main/java/edu/cad/entities/SubjectStudyLoadSubmitted.java
@@ -0,0 +1,31 @@
+package edu.cad.entities;
+
+import edu.cad.documentelements.k3columns.StudyLoadType;
+import edu.cad.entities.interfaces.IDatabaseEntity;
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Data
+@Entity
+@Table(name = "subject_study_load_submitted")
+public class SubjectStudyLoadSubmitted implements IDatabaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "subject_load_id")
+    private SubjectStudyLoad targetLoad;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "staff_id")
+    private Staff assignedProfessor;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "type")
+    private StudyLoadType studyLoadType;
+
+    @Column(name = "value")
+    private double amount;
+}
