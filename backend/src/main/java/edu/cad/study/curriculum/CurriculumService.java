a4cbd6caacf86f6a48ef6a15909871b2a96f5dab
buhaiovos <oleksandr.buhaiov@gmail.com>
2020-02-15 17:01:32 +0200

    add CurriculumSubjectService
    * extract some low-level object handling into a separate service

--- backend/src/main/java/edu/cad/study/curriculum/CurriculumService.java
+++ backend/src/main/java/edu/cad/study/curriculum/CurriculumService.java
@@ -2,9 +2,7 @@ package edu.cad.study.curriculum;
 
 import edu.cad.entities.Curriculum;
 import edu.cad.entities.CurriculumSubject;
-import edu.cad.entities.SubjectInfo;
 import edu.cad.study.EntityService;
-import edu.cad.study.subject.info.SubjectInfoService;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
@@ -12,20 +10,16 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
-import java.util.Map;
 import java.util.Optional;
 import java.util.Set;
-
-import static java.util.stream.Collectors.toMap;
-import static java.util.stream.Collectors.toSet;
 
 @Service
 @RequiredArgsConstructor
 @FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
 @Slf4j
 public class CurriculumService implements EntityService<Curriculum, Integer, CurriculumDto> {
+    CurriculumSubjectService curriculumSubjectService;
     CurriculumRepositoryWrapper repo;
-    SubjectInfoService subjectInfoService;
 
     @Override
     public List<Curriculum> getAll() {
@@ -43,7 +37,7 @@ public class CurriculumService implements EntityService<Curriculum, Integer, Cur
         final var newCurriculum = repo.save(curriculum);
 
         Set<CurriculumSubject> curriculumSubjects =
-                getCurriculumSubjects(curriculumDto, newCurriculum);
+                curriculumSubjectService.getCurriculumSubjects(curriculumDto.getSubjectIdsByCiphers(), newCurriculum);
 
         newCurriculum.setCurriculumSubjects(curriculumSubjects);
         return repo.save(newCurriculum);
@@ -54,29 +48,12 @@ public class CurriculumService implements EntityService<Curriculum, Integer, Cur
         var curriculum = repo.findById(id).orElseThrow();
         curriculum.setDenotation(curriculumDto.getDenotation());
 
-        Set<CurriculumSubject> curriculumSubjects = getCurriculumSubjects(curriculumDto, curriculum);
+        Set<CurriculumSubject> curriculumSubjects =
+                curriculumSubjectService.getCurriculumSubjects(curriculumDto.getSubjectIdsByCiphers(), curriculum);
 
         curriculum.setCurriculumSubjects(curriculumSubjects);
 
         return curriculum;
-    }
-
-    private Set<CurriculumSubject> getCurriculumSubjects(CurriculumDto curriculumDto, Curriculum curriculum) {
-        Map<Integer, SubjectInfo> subjectsById =
-                subjectInfoService.getByIds(List.copyOf(curriculumDto.getSubjectIdsByCiphers().values()))
-                        .stream()
-                        .collect(toMap(SubjectInfo::getId, i -> i));
-
-        return curriculumDto.getSubjectIdsByCiphers().entrySet()
-                .stream()
-                .map(entry -> {
-                    var curriculumSubject = new CurriculumSubject();
-                    curriculumSubject.setCipher(entry.getKey());
-                    curriculumSubject.setCurriculum(curriculum);
-                    curriculumSubject.setSubjectInfo(subjectsById.get(entry.getValue()));
-                    return curriculumSubject;
-                })
-                .collect(toSet());
     }
 
     @Override
