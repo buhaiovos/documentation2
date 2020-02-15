eddf0bc99ca740d46a10125c4e434ebb0f7cb6a7
buhaiovos <oleksandr.buhaiov@gmail.com>
2020-02-15 18:10:24 +0200

    remove unnecessary beans

--- backend/src/main/java/edu/cad/configuration/DocumentationBeans.java
+++ backend/src/main/java/edu/cad/configuration/DocumentationBeans.java
@@ -1,9 +1,5 @@
 package edu.cad.configuration;
 
-import edu.cad.daos.DistributedStudyLoadDao;
-import edu.cad.daos.HibernateDao;
-import edu.cad.daos.OtherLoadInfoDao;
-import edu.cad.entities.*;
 import edu.cad.services.years.DbYearsTrackingService;
 import edu.cad.utils.databaseutils.DatabaseCloner;
 import edu.cad.utils.databaseutils.DatabaseSwitcher;
@@ -20,35 +16,5 @@ public class DocumentationBeans {
                 new DatabaseCloner(),
                 yearsTrackingService
         );
-    }
-
-    @Bean
-    public HibernateDao<SubjectStudyLoad> subjectStudyLoadHibernateDao() {
-        return new HibernateDao<>(SubjectStudyLoad.class);
-    }
-
-    @Bean
-    public HibernateDao<Staff> staffHibernateDao() {
-        return new HibernateDao<>(Staff.class);
-    }
-
-    @Bean
-    public OtherLoadInfoDao otherLoadInfoHibernateDao() {
-        return new OtherLoadInfoDao();
-    }
-
-    @Bean
-    public HibernateDao<OtherLoad> otherLoadHibernateDao() {
-        return new HibernateDao<>(OtherLoad.class);
-    }
-
-    @Bean
-    public HibernateDao<DistributedOtherLoadInfo> otherLoadInfoSubmittedHibernateDao() {
-        return new HibernateDao<>(DistributedOtherLoadInfo.class);
-    }
-
-    @Bean
-    public DistributedStudyLoadDao distributedStudyLoadDao() {
-        return new DistributedStudyLoadDao(DistributedSubjectStudyLoad.class);
     }
 }
