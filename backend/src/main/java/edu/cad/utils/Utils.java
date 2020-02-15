175fb3d05e32c8ec503a7bbbdef08efd6535fa4c
buhaiovos <oleksandr.buhaiov@gmail.com>
2020-02-15 17:39:24 +0200

    handy elvis-like (?.) util

--- backend/src/main/java/edu/cad/utils/Utils.java
+++ backend/src/main/java/edu/cad/utils/Utils.java
@@ -1,20 +1,27 @@
 package edu.cad.utils;
 
+import lombok.experimental.UtilityClass;
 import org.apache.commons.lang3.math.NumberUtils;
 
 import java.util.OptionalInt;
+import java.util.function.Function;
 
+import static java.util.Optional.ofNullable;
+
+@UtilityClass
 public class Utils {
-    private Utils() {
-        // utility classes should not be instantiated.
-    }
-
     public static boolean isNumber(String value) {
         return NumberUtils.isParsable(value);
     }
 
     public static OptionalInt parseInteger(String value) {
         return NumberUtils.isParsable(value) ? OptionalInt.of(Integer.valueOf(value)) : OptionalInt.empty();
+    }
+
+    public static <T, R> R nullOr(T obj, Function<T, R> getter) {
+        return ofNullable(obj)
+                .map(getter)
+                .orElse(null);
     }
 }
 
