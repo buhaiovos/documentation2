0eb6a891f506877fccdd290d172c181a6c85c0c0
buhaiovos <oleksandr.buhaiov@gmail.com>
2020-04-27 21:43:22 +0300

    [CAD-0000] Set up security config for development

--- backend/src/main/java/edu/cad/configuration/properties/DevSecurityConfiguration.java
+++ backend/src/main/java/edu/cad/configuration/properties/DevSecurityConfiguration.java
@@ -6,17 +6,12 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration
-@Profile("!dev")
-public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+@Profile("dev")
+public class DevSecurityConfiguration extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
-                .csrf().disable()
                 .authorizeRequests()
-                .anyRequest().authenticated()
-                .and()
-                .formLogin().permitAll()
-                .and()
-                .logout().permitAll();
+                .anyRequest().permitAll();
     }
 }
