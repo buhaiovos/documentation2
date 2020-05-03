62869e7e38e0a6b328f004df7b9571a007cef2a2
buhaiovos <oleksandr.buhaiov@gmail.com>
2020-05-03 17:38:35 +0300

    [CAD-0000] fix cors for dev profile, move files

--- backend/src/main/java/edu/cad/configuration/DevSecurityConfiguration.java
+++ backend/src/main/java/edu/cad/configuration/DevSecurityConfiguration.java
@@ -1,17 +1,15 @@
-package edu.cad.configuration.properties;
+package edu.cad.configuration;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration
 @Profile("dev")
 public class DevSecurityConfiguration extends WebSecurityConfigurerAdapter {
     @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http
-                .authorizeRequests()
-                .anyRequest().permitAll();
+    public void configure(WebSecurity web) {
+        web.ignoring().anyRequest();
     }
 }
